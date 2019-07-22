ea7f4c909fc4f6fdc22e92ec0c5b3446badbb57d
satriakurniawan16 <rahmadsatria16@gmail.com>
2019-07-22 13:41:00 +0700

    "firstCommit"

---
+++ app/src/main/java/com/mvvm/agv3/agv3mvvm/View/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.mvvm.agv3.agv3mvvm.View;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.mvvm.agv3.agv3mvvm.MainActivity;
+import com.mvvm.agv3.agv3mvvm.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        Handler handler = new Handler();
+        final Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler.postDelayed(r,7000);
+
+    }
+}
