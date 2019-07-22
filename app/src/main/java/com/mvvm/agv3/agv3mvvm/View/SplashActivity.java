e3bb5f3ba6af597f83defa129b2be56915fbc0e0
satriakurniawan16 <rahmadsatria16@gmail.com>
2019-07-22 13:45:04 +0700

    "firstCommit"

--- app/src/main/java/com/mvvm/agv3/agv3mvvm/View/SplashActivity.java
+++ app/src/main/java/com/mvvm/agv3/agv3mvvm/View/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+//ddd
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
